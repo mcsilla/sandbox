ee1304f1148ba555bb35ebfc3502338f58a8d847
Majoros Csilla <majoroscsilla88@gmail.com>
2019-01-10 12:59:09 +0100

    /count counts visitors of index.html

--- flask/server.py
+++ flask/server.py
@@ -1,16 +1,25 @@
 from flask import Flask
 app = Flask(__name__)
 
+counter = 0
+
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
 @app.route('/index.html')
 def content():
+    global counter
+    counter += 1
     text = open('flask/index.html')
     content = text.read()
     text.close()
     return content
+
+
+@app.route('/count')
+def visitors():
+    return str(counter)
 
 if __name__ == '__main__':
     app.run(debug=True, port=2001, host='0.0.0.0')
