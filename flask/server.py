4c80068eade5e1a39224be3cefc835ce97b35e4d
Majoros Csilla <majoroscsilla88@gmail.com>
2019-01-10 12:02:40 +0100

    display content of index.html

--- flask/server.py
+++ flask/server.py
@@ -5,5 +5,12 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World!'
 
+@app.route('/index.html')
+def content():
+    text = open('flask/index.html')
+    content = text.read()
+    text.close()
+    return content
+
 if __name__ == '__main__':
     app.run(debug=True, port=2001, host='0.0.0.0')
