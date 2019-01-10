c03865762a4dc26ae02962c1ea66cac2514a0c35
Majoros Csilla <majoroscsilla88@gmail.com>
2019-01-10 13:38:59 +0100

    counter.txt added

--- flask/server.py
+++ flask/server.py
@@ -1,16 +1,21 @@
 from flask import Flask
 app = Flask(__name__)
 
-counter = 0
-
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
+counter_file = open('flask/counter.txt', 'r')
+actual_counter = int(counter_file.read(1))
+counter_file.close()
+
 @app.route('/index.html')
 def content():
-    global counter
-    counter += 1
+    global actual_counter
+    actual_counter += 1
+    counter_file = open('flask/counter.txt', 'w')
+    counter_file.write(str(actual_counter))
+    counter_file.close()
     text = open('flask/index.html')
     content = text.read()
     text.close()
@@ -19,7 +24,7 @@ def content():
 
 @app.route('/count')
 def visitors():
-    return str(counter)
+    return str(actual_counter)
 
 if __name__ == '__main__':
     app.run(debug=True, port=2001, host='0.0.0.0')
